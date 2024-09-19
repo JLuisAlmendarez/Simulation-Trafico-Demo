95af9d14399b91ebcaedc89bd9e3879e9358dbb2
Jose Almendarez <almendarez1002@gmail.com>
2024-09-19 11:16:24 -0600

    Primer commit

---
+++ demo/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<h1>Simulación del Trafico de Chapala</h1>
+<div>
+    <div>
+        <button>Modelo(Muestra el mapa con las calles)</button>
+        <button>Simulación</button>
+    </div>
+    <br>
+    <div>
+        <div>
+            <p>En esta parte estara el mapa</p>
+        </div>
+        <div>
+            <p>En esta parte estara para introducir semaforos</p>
+            <p>Esta parte es para introducir incidentes </p>
+        </div>
+        <button>Correr Simulacion</button>
+        <div>Esta parte es para servir la analitica</div>
+    </div>
+</div>
